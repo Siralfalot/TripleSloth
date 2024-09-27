e693c9d81df279a26c87ff19a173e9ddf00b612d
Siralfalot <132355629+Siralfalot@users.noreply.github.com>
2024-09-27 16:26:30 +0100

    First Push

---
+++ Assets/Scripts/Minigame1/Paths.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paths : MonoBehaviour
+{
+    public float[] pathPositions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
