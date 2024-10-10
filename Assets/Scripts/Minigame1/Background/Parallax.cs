5e4bce6d7644320e1ae51c7ab0e9826ad4e7a639
abertayray <2104143@uad.ac.uk>
2024-10-10 23:50:35 +0100

    Parallaxing background

--- Assets/Scripts/Minigame1/Background/Parallax.cs
+++ Assets/Scripts/Minigame1/Background/Parallax.cs
@@ -1,3 +1,4 @@
+using SimpleExampleGame;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,39 @@ public class Parallax : MonoBehaviour
     public bool canScroll = true;
     private Vector3 startPosition;
     private float textureHeight;
+    public int layer;
+    public Minigame1 minigameRef;
+    Sprite sprite;
+    SpriteRenderer spriteRenderer;
 
-    void Start()
+    private void Awake()
     {
-        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        minigameRef.MinigameLoaded.AddListener(Initialise);
+        minigameRef.MinigameFinished.AddListener(DisableBG);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        sprite = spriteRenderer.sprite;
+        
+        spriteRenderer.enabled = false;
+    }
+
+    void Initialise()
+    {
+        spriteRenderer.enabled = true;
         textureHeight = (sprite.texture.height / sprite.pixelsPerUnit)*transform.localScale.y;
 
-        startPosition = transform.position;
+        //startPosition = transform.position;
+        startPosition = Vector3.zero;
+
+        SetScrollSpeed(layer);
+    }
+
+    void DisableBG()
+    {
+        spriteRenderer.enabled = false;
+
+        textureHeight = (sprite.texture.height / sprite.pixelsPerUnit) * transform.localScale.y;
+
+        SetScrollSpeed(layer);
     }
 
     // Update is called once per frame
@@ -29,6 +56,27 @@ public class Parallax : MonoBehaviour
         if ((Mathf.Abs(transform.position.y) - textureHeight) > 0)
         {
             transform.position = new Vector3(transform.position.x, startPosition.y, transform.position.z);
+            Debug.Log("ScrollBG reset");
+        }
+    }
+
+    private void SetScrollSpeed(int layer)
+    {
+        switch (layer)
+        {
+            case 1:
+                scrollSpeed = minigameRef.gameSpeed;
+                break;
+            case 2:
+                scrollSpeed = minigameRef.gameSpeed - 1f;
+                break;
+            case 3:
+                scrollSpeed = minigameRef.gameSpeed - 1.5f;
+                break;
+            default:
+                scrollSpeed = 0f;
+                Debug.LogWarning("ScrollSpeed not set. Layer missing.");
+                break;
         }
     }
 }
